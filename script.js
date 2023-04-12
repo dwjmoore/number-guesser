84f6893a08d3776b250b38504af33394c6e186ec
David Moore <dwjmoore@gmail.com>
2023-04-12 21:09:49 +0000

    created compare guesses function

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ let currentRoundNumber = 1;
 const generateTarget = () => {
 	return Math.floor(Math.random() * 10);
 };
+
+const compareGuesses = (userGuess, computerGuess, targetNumber) => {
+	const userDifference = Math.abs(targetNumber - userGuess);
+	const computerDifference = Math.abs(targetNumber - computerGuess);
+
+	if (userDifference < computerDifference) {
+		return true;
+	}
+	if (userDifference > computerDifference) {
+		return false;
+	}
+	return true;
+};
+
+
+/*
+
+
+*/
