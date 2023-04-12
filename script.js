c17d3a69c7367d38965647a3d7b486178a6e035d
David Moore <dwjmoore@gmail.com>
2023-04-12 21:24:17 +0000

    create get absolute difference function to use in compare guesses function

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ const generateTarget = () => {
 };
 
 const compareGuesses = (userGuess, computerGuess, targetNumber) => {
-	const userDifference = Math.abs(targetNumber - userGuess);
-	const computerDifference = Math.abs(targetNumber - computerGuess);
+	const userDifference = getAbsoluteDistance(userGuess, targetNumber);
+	const computerDifference = getAbsoluteDistance(computerGuess, targetNumber);
 
 	if (userDifference < computerDifference) {
 		return true;
@@ -31,3 +31,13 @@ const updateScore = (winner) => {
 const advanceRound = () => {
 	currentRoundNumber += 1;
 }
+
+const getAbsoluteDistance = (num1, num2) => {
+	return Math.abs(num1 - num2);
+}
+
+/*
+
+You probably calculated the distance from the computer guess to the target and from the human guess to the target. Move this into a separate getAbsoluteDistance() function that takes two numbers and returns the distance, and then use that inside your compareGuesses() function.
+Add functionality to check whether the user guess is between 0 and 9 and alert() the user that their number is out of range. It’s not possible to set a number outside this range with the + and = buttons, but users can do so by typing directly in the input field.
+*/
